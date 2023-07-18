b836bfde4bc53697455698dbf561fcc9db5b0ea1
santimber <santiago.g.del@gmail.com>
2023-07-18 09:57:09 +0200

    new bot new bot

--- ZPU_improved.py
+++ ZPU_improved.py
@@ -74,7 +74,7 @@ chain = load_chain()
 # setting up streamlit
 
 st.set_page_config(page_title="PaCa chatbot Demo", page_icon=":robot:")
-st.header("LangChain Demo")
+st.header("Paca Bot")
 
 if "generated" not in st.session_state:
     st.session_state["generated"] = []
@@ -83,7 +83,7 @@ if "past" not in st.session_state:
     st.session_state["past"] = []
 
 def get_text():
-    input_text = st.text_input("You: ", "Hello, how are you?", key="input")
+    input_text = st.text_input("You: ", "Hello, I have some questions about the platform rules", key="input")
     return input_text
 
 user_input = get_text()
@@ -95,7 +95,6 @@ if user_input:
     st.session_state.generated.append(output)
 
 if st.session_state["generated"]:
-
     for i in range(len(st.session_state["generated"]) - 1, -1, -1):
         message(st.session_state["generated"][i], key=str(i))
         message(st.session_state["past"][i], is_user=True, key=str(i) + "_user")
